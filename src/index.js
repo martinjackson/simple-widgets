c2ff25ea69b607d900d19a2b7a93fa634da81876
Martin A. Jackson <martin.a.jackson@gmail.com>
2018-09-04 11:38:27 -0500

    intial build

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+let SimpleWidgets = {}
+SimpleWidgets.CheckBox = require('./CheckBox')
+SimpleWidgets.Choice = require('./Choice')
+SimpleWidgets.Radio = require('./Radio')
+
+module.exports = SimpleWidgets
