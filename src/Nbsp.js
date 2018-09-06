ebce8b93b9156ef4c3cabacfd421c7fcb80f6449
Martin A. Jackson <martin.a.jackson@gmail.com>
2018-09-06 13:57:53 -0500

    working thru testing

---
+++ src/Nbsp.js
@@ -0,0 +1,8 @@
+
+
+   const Nbsp1 = <div dangerouslySetInnerHTML={{__html: '&nbsp;'}} />
+
+   const space = '\u00A0'
+   const Nbsp2 = <span>{space}</span>
+
+   
---
+++ test/labelSpec.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactTestUtils from 'react-dom/test-utils';
+import Label from '../src/Label'
+
+describe('Label', function () {
+
+  it('should not be undefined', function () {
+    var Component = ReactTestUtils.renderIntoDocument(<Label />);
+    var element = ReactTestUtils.findRenderedDOMComponentWithTag(Component, 'div');
+    expect(element).toBeTruthy();
+  });
+
+});
+ 
