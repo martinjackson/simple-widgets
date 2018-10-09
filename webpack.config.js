9a23eb1b0277e952e3711d021231625a4d293476
Martin Jackson <martin.a.jackson@gmail.com>
2018-10-09 01:12:08 -0500

    working version, needs debug log removed

---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+
+const path = require('path');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const pkg = require('./package.json');
+const libraryName= pkg.name;  
+
+module.exports = {
+  entry: path.join(__dirname, "./src/index.js"),
+  target: "web",
+  output: {
+    path: path.join(__dirname, 'lib'),
+    filename: "index.js",
+    library: libraryName,      
+    libraryTarget: 'umd',      
+    publicPath: '/lib/',      
+    umdNamedDefine: true  
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, "src"),
+      "node_modules"
+    ]
+  },
+  devtool: "source-map",
+  stats: "normal",
+  plugins: [
+    new ProgressBarPlugin(),
+  ],
+  module: {
+    rules: [
+          {
+            test: /^(?!.*\.{test,min}\.js$).*\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                      presets: ['@babel/preset-env', '@babel/preset-react']
+                   },
+          },
+
+          {
+            test: /\.tsx?$/,
+            loader: "awesome-typescript-loader",
+          },
+          
+          {
+              test: /\.css$/,
+              use: [ 'style-loader', 'css-loader' ]
+          },
+
+          // "file" loader for svg
+          {
+             test: /\.svg|\.png|\.gif|\.jpg$/,
+             loader: 'file-loader',
+             query: {
+               name: 'static/media/[name].[hash:8].[ext]'
+             }
+          },
+
+          {
+            test: /\.html$/,
+            loader: 'raw-loader'
+          }
+    ]
+  },
+  resolve: {      
+    alias: {          
+        'react': path.resolve(__dirname, './node_modules/react'),
+        'react-dom': path.resolve(__dirname, './node_modules/react-dom'),      
+    }  
+},  
+externals: {      
+    // Don't bundle react or react-dom      
+    react: {          
+        commonjs: "react",          
+        commonjs2: "react",          
+        amd: "React",          
+        root: "React"      
+    },      
+    "react-dom": {          
+        commonjs: "react-dom",          
+        commonjs2: "react-dom",          
+        amd: "ReactDOM",          
+        root: "ReactDOM"      
+    }  
+}
+}
