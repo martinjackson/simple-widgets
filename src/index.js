ec931fcd7e0588bb65ac0c0566a82346704ed7bd
Martin Jackson <martin.a.jackson@gmail.com>
2018-10-31 05:34:40 -0500

    no tests yet for new components submitted by Jim

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 
+// functions
+import makeChangeHandler from './makeChangeHandler'
+
+// React.Components
 import CheckBox from './CheckBox'
 import Choice from './Choice'
 import Radio  from './Radio'
 import DatePicker  from './DatePicker'
-import makeChangeHandler from './makeChangeHandler'
+import DoubleListBox  from './DoubleListBox'
+import {SelectionPanel1, SelectionPanel2} from './SelectionPanels'
 
-export {CheckBox, Choice, Radio, DatePicker, makeChangeHandler}
+
+export { CheckBox, Choice, Radio, DatePicker, makeChangeHandler, DoubleListBox, SelectionPanel1, SelectionPanel2 }
