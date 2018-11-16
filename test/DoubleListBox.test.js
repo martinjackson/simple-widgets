628eaabb27d3eae45390fc34ef7cabc0e9e6ef11
Martin A. Jackson <martin.a.jackson@gmail.com>
2018-11-16 14:58:21 -0600

    testing use of react-beautiful-dnd version of DoubleListBox instead of DoubleListBoxJim

--- test/DoubleListBox.test.js
+++ test/DoubleListBox.test.js
@@ -3,6 +3,7 @@ import Enzyme, { mount, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 import DoubleListBox from '../src/DoubleListBox';
+import makeChangeHandler from '../src/makeChangeHandler'
 
 Enzyme.configure({ adapter: new Adapter() })
 
@@ -12,50 +13,16 @@ const preSelected = list.filter( item => item.startsWith('b') )
 class DLBTest extends React.Component {
   constructor(props) {
     super(props);
-
-    this.options = [];    // The entire list of fields (columns) that can be in the report
-    this.selections = []; // The list of fields (columns) the user wants in the report
-
-    // Build the options array for the left list box
-    for (let i = 0; i < list.length; i++) {
-       this.options.push ({label: list[i], value: i });
-    }
-    
+    this.state = {
+      fruitChoice: this.props.preselected
+    };
+    this.handleChange = makeChangeHandler(this);   
   }
 
   render() {
-    return <DoubleListBox className="ms-selection" options={this.options} isSelected="true" onChange={this.selection} />
+    return <DoubleListBox choices={list} name="fruitChoice" value={this.state.fruitChoice} onChange={this.handleChange} />
   }
 
-      /***************************************************************************
-     *
-     *  This will place the values in right list box in the selections array.
-     *
-     *  @param selectedValues the array of value fields in the options array of
-     *                        the selected values.  For example, consider the
-     *                        following options array:
-     *                          label: John     value: 11
-     *                          label: Pete     value: 22
-     *                          label: Paul     value: 33
-     *                          label: Luke     value: 44
-     *                        Suppose the selected values array contains 22 and
-     *                        33.  This indicates that Pete and Paul were
-     *                        selected.
-     *
-     **************************************************************************/
-    selection(selectedValues) {
-        const answer = [];
-   
-        for (let i = 0; i < selectedValues.length; i++) {
-          for (let j = 0; j < this.options.length; j++) {
-            if (selectedValues[i] === this.options[j].value) {
-              answer.push (this.options[j].label);
-            }
-          }
-
-          this.selections = answer;
-        }
-      }
 }
 
 
@@ -66,5 +33,25 @@ it('renders without crashing', () => {
 it('renders with preselection', () => {    
     console.log(preSelected);
     
-    const wrapper = mount(<DLBTest selected={preSelected} />)
+    const wrapper = mount(<DLBTest preselected={preSelected} />)
 });
+
+it('gets selected values', () => {      
+  const wrapper = mount(<DLBTest preselected={preSelected} />)
+  const comp = wrapper.instance()
+  expect(wrapper.state('fruitChoice')).toEqual(preSelected)
+});
+
+it('select all', () => {      
+  const wrapper = mount(<DLBTest preselected={preSelected} />)
+  
+  const innerWrapper = wrapper.find(DoubleListBox)
+  const comp = innerWrapper.instance()
+  
+  comp.allSelect();
+  const ans = wrapper.state('fruitChoice')
+  console.log('ans:', ans);
+  
+  expect(ans).toEqual(preSelected)
+});
+    
