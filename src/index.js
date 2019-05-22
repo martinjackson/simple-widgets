23586a504fdd1c6442d2edb9170f900a42c8f1e5
Martin Jackson <martin.a.jackson@gmail.com>
2019-05-22 09:29:48 -0500

    folding in simpler DoubleListBox -- thanks Jim!

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import makeChangeHandler from './makeChangeHandler'
 
 // React.Components
 import CheckBox from './CheckBox'
-import Choice from './Choice'
+import {Choice, List} from './List'
 import Radio  from './Radio'
 import DatePicker  from './DatePicker'
-import DoubleListBox  from './DoubleListBox/DoubleListBox'
+import DoubleListBox  from './DoubleListBox'
 
 
-export { CheckBox, Choice, Radio, DatePicker, makeChangeHandler, DoubleListBox}
+export { makeChangeHandler, CheckBox, Choice, List, Radio, DatePicker, DoubleListBox }
