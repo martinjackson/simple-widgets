1f5b736fd699e35f9d9bf5d5d0003ac92c71a68e
Martin A. Jackson <martin.a.jackson@gmail.com>
2020-11-12 09:23:19 -0600

    corrected  ConfirmModal import and added SearchSortTable

--- src/index.js
+++ src/index.js
@@ -10,11 +10,13 @@ import Radio  from './Radio'
 import DatePicker  from './DatePicker'
 import DoubleListBox  from './DoubleListBox'
 
-import AlertModal from './AlertModal.js'
-import ConfirmMocal from './ConfirmModal.js'
-import ErrorModal from './ErrorModal.js'
-import Modal from './Modal.js'
-import Theme from './Theme.js'
+import AlertModal from './AlertModal'
+import ConfirmModal from './ConfirmModal'
+import ErrorModal from './ErrorModal'
+import Modal from './Modal'
+import Theme from './Theme'
+
+import SearchSortTable from './SearchSortTable'
 
 import { setInvalidScreen, setInvalidTable, setInvalidDual, resetDisplayScreen, resetDisplayTable, wasClickedScreen, wasClickedTable,
    isConstant, validCheckDual, validCheckScreen, validCheckTable, clearInvalidDual, clearInvalidScreen, clearInvalidTable, copyStyle,
@@ -23,9 +25,10 @@ import { setInvalidScreen, setInvalidTable, setInvalidDual, resetDisplayScreen, 
 
 export {
    makeChangeHandler, CheckBox, Choice, ChoiceText, List, Radio, DatePicker, DoubleListBox,
-   AlertModal, ConfirmMocal, ErrorModal, Modal, Theme,
+   AlertModal, ConfirmModal, ErrorModal, Modal, Theme,
    setInvalidScreen, setInvalidTable, setInvalidDual, resetDisplayScreen, resetDisplayTable, wasClickedScreen, wasClickedTable,
    isConstant, validCheckDual, validCheckScreen, validCheckTable, clearInvalidDual, clearInvalidScreen, clearInvalidTable, copyStyle,
-   processStyleScreen, processStyleTable, validStyling, invalidStyling, isInvalid, getInvalidMessage
+   processStyleScreen, processStyleTable, validStyling, invalidStyling, isInvalid, getInvalidMessage,
+   SearchSortTable
 }
 
