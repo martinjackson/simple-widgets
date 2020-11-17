5ef55efea8ff55308f82e49f06a1c71af00a145b
Martin A. Jackson <martin.a.jackson@gmail.com>
2020-11-17 16:14:27 -0600

    working on auto-gen src/index.js to avoid typos

---
+++ src/gen_imports.js
@@ -0,0 +1,25 @@
+
+const fs = require('fs');
+
+var files = fs.readdirSync('.')
+              .filter(fn => fn.endsWith('.js'))
+              .filter(fn => fn !== 'gen_imports.js')
+              .filter(fn => fn !== 'index.js')
+
+const ans = files.map( fname => {
+  const names = fs.readFileSync(fname,{ encoding : 'utf8' })
+      .split('\n')
+      .filter( line => line.includes('export '))
+      .filter( line => !line.includes('export default'))
+      .map(line => line.split(' ')[2])  // get 3rd word
+
+  return { fname, names:names.join(', ') }
+})
+   .filter(f => f.names.length>0)
+   .map( info => [
+     `import { ${info.names} } from '${info.fname}' `,
+     `export { ${info.names} }`
+   ])
+
+console.log(ans.flat());
+
