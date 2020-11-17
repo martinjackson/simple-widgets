a24cbdadba9f5bcd9d9b9a4d34fc02f6aa4c96b4
Martin A. Jackson <martin.a.jackson@gmail.com>
2020-11-17 16:30:32 -0600

    cleanup src/index.js

--- src/index.js
+++ src/index.js
@@ -16,15 +16,23 @@ import Modal from './Modal'
 
 import SearchSortTable from './SearchSortTable'
 
-export {CheckBox, ChoiceText, Radio, DatePicker, DoubleListBox, AlertModal, ConfirmModal, ErrorModal, Modal, SearchSortTable}
+export {CheckBox, ChoiceText, Radio, DatePicker, DoubleListBox, AlertModal, ConfirmModal,
+        ErrorModal, Modal, SearchSortTable}
 
 export { Choice, List }
 export { DatePickerStyle() }
 export { defaultThemeSettings, buttonStyle, generateButton, generateDefaultButton }
-export { setInvalidScreen, setInvalidTable, setInvalidDual, resetDisplayScreen, resetDisplayTable, wasClickedScreen, wasClickedTable, isConstant, validCheckDual, validCheckScreen, validCheckTable, clearInvalidDual, clearInvalidScreen, clearInvalidTable, copyStyle, processStyleScreen, clearInvalidScreenOnly, processStyleTable, validStyling, invalidStyling, isInvalid, getInvalidMessage }
+export { setInvalidScreen, setInvalidTable, setInvalidDual, resetDisplayScreen, resetDisplayTable,
+         wasClickedScreen, wasClickedTable, isConstant, validCheckDual, validCheckScreen, validCheckTable,
+         clearInvalidDual, clearInvalidScreen, clearInvalidTable, copyStyle, processStyleScreen,
+         clearInvalidScreenOnly, processStyleTable, validStyling, invalidStyling, isInvalid, getInvalidMessage }
 
 import { Choice, List } from 'List.js'
 import { DatePickerStyle() } from 'DatePickerStyle.js'
 import { defaultThemeSettings, buttonStyle, generateButton, generateDefaultButton } from 'Theme.js'
-import { setInvalidScreen, setInvalidTable, setInvalidDual, resetDisplayScreen, resetDisplayTable, wasClickedScreen, wasClickedTable, isConstant, validCheckDual, validCheckScreen, validCheckTable, clearInvalidDual, clearInvalidScreen, clearInvalidTable, copyStyle, processStyleScreen, clearInvalidScreenOnly, processStyleTable, validStyling, invalidStyling, isInvalid, getInvalidMessage } from 'Invalid.js'
+import { setInvalidScreen, setInvalidTable, setInvalidDual, resetDisplayScreen, resetDisplayTable,
+         wasClickedScreen, wasClickedTable, isConstant, validCheckDual, validCheckScreen, validCheckTable,
+         clearInvalidDual, clearInvalidScreen, clearInvalidTable, copyStyle, processStyleScreen,
+         clearInvalidScreenOnly, processStyleTable, validStyling, invalidStyling, isInvalid,
+         getInvalidMessage } from 'Invalid.js'
 
