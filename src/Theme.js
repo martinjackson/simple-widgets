b5416a4d50172c441ae12b178ed308f3cb1439be
Martin A. Jackson <martin.a.jackson@gmail.com>
2020-11-18 16:06:37 -0600

    Release 1.7.0

--- src/Theme.js
+++ src/Theme.js
@@ -1,7 +1,23 @@
 /***********************************************************************************
- * 
+ *
+ * The default button style.
+ *
+ ***********************************************************************************/
+export const buttonStyle = {
+  margin: "10px",
+  borderRadius: "10px",
+  color: defaultThemeSettings.buttonTextColor,
+  backgroundColor: defaultThemeSettings.buttonColor,
+  width: "100px",
+  height: "30px",
+  font: defaultThemeSettings.buttonFont,
+  fontWeight: "bold",
+};
+
+/***********************************************************************************
+ *
  * The default theme settings.
- * 
+ *
  ***********************************************************************************/
 export const defaultThemeSettings = {
     backgroundColor: 'aliceblue',
@@ -17,44 +33,30 @@ export const defaultThemeSettings = {
     errorColor: 'pink',
     normalColor: 'white',
     textColor: 'black',
+    buttonStyle: buttonStyle,
 };
 
-/***********************************************************************************
- * 
- * The default button style.
- * 
- ***********************************************************************************/
-export const buttonStyle = {
-    margin: "10px",
-    borderRadius: "10px",
-    color: defaultThemeSettings.buttonTextColor,
-    backgroundColor: defaultThemeSettings.buttonColor,
-    width: "100px",
-    height: "30px",
-    font: defaultThemeSettings.buttonFont,
-    fontWeight: "bold",
-};
 
 
 /*********************************************************************
- * 
+ *
  * 04-23-2020   Initial check-in.
- * 09-16-2020   Added the getNormalBackColor and getErrorColor 
+ * 09-16-2020   Added the getNormalBackColor and getErrorColor
  *              functions.
- * 
+ *
  *********************************************************************/
 
 /********************************************************************************
- * 
+ *
  * This will generate a button and will determine if the button should be
  * disabled or not.
- * 
- * @param {*} style         the style used for the button 
- * @param {*} error         indicates whether an error occurred and if so, disable 
+ *
+ * @param {*} style         the style used for the button
+ * @param {*} error         indicates whether an error occurred and if so, disable
  *                          the button
  * @param {*} disabled      indicates whether the button should be disabled or not
  * @param {*} disableColor  the color for the disabled button (default is gray)
- * 
+ *
  *********************************************************************************/
 export const generateButton = (style, error, disabled = false, disableColor = 'gray') => {
     const genButtonStyle = Object.assign ({}, style); // Copy the button style
@@ -62,7 +64,7 @@ export const generateButton = (style, error, disabled = false, disableColor = 'g
         genButtonStyle.backgroundColor = disableColor;
     } else {    // Do not disable the button
         // Make sure the style has a background color, if not use the theme color
-        genButtonStyle.backgroundColor = (style.hasOwnProperty('backgroundColor') === true) ? 
+        genButtonStyle.backgroundColor = (style.hasOwnProperty('backgroundColor') === true) ?
             style.backgroundColor : defaultThemeSettings.backgroundColor;
     }
 
@@ -70,14 +72,14 @@ export const generateButton = (style, error, disabled = false, disableColor = 'g
 }
 
 /********************************************************************************
- * 
+ *
  * This will generate a button and will determine if the button should be
  * disabled or not.
- * 
- * @param {*} error         indicates whether an error occurred and if so, disable 
+ *
+ * @param {*} error         indicates whether an error occurred and if so, disable
  *                          the button
  * @param {*} disabled      indicates whether the button should be disabled or not
- * 
+ *
  *********************************************************************************/
 export const generateDefaultButton = (error, disabled = false) => {
     const genButtonStyle = Object.assign ({}, buttonStyle); // Copy the button style
