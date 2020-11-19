05d13389b0a6a7231a193f345066a31822f7a29d
James Abele <James.Abele@fda.hhs.gov>
2020-11-19 15:30:46 -0600

    Synching back up.

--- src/Theme.js
+++ src/Theme.js
@@ -1,60 +1,70 @@
+
+const hasProperty = (obj, propName) => { return !!Object.getOwnPropertyDescriptor(obj, propName);}
+
 /***********************************************************************************
- * 
+ *
  * The default theme settings.
- * 
+ *
  ***********************************************************************************/
-export const defaultThemeSettings = {
-    backgroundColor: 'aliceblue',
-    buttonColor: 'blue',
-    buttonTextColor: 'white',
-    buttonFont: 'Times New Roman',
-    buttonFontWeight: 'bold',
-    buttonFontSize: 12,
-    disableButtonColor: 'gray',
-    font: 'Times New Roman',
-    fontWeight: 'plain',
-    fontSize: 12,
-    errorColor: 'pink',
-    normalColor: 'white',
-    textColor: 'black',
+const defaultSettings = {
+  backgroundColor: 'aliceblue',
+  buttonColor: 'blue',
+  buttonTextColor: 'white',
+  buttonFont: 'Times New Roman',
+  buttonFontWeight: 'bold',
+  buttonFontSize: 12,
+  disableButtonColor: 'gray',
+  font: 'Times New Roman',
+  fontWeight: 'plain',
+  fontSize: 12,
+  errorColor: 'pink',
+  normalColor: 'white',
+  textColor: 'black',
 };
 
 /***********************************************************************************
- * 
+ *
  * The default button style.
- * 
+ *
  ***********************************************************************************/
 export const buttonStyle = {
-    margin: "10px",
-    borderRadius: "10px",
-    color: defaultThemeSettings.buttonTextColor,
-    backgroundColor: defaultThemeSettings.buttonColor,
-    width: "100px",
-    height: "30px",
-    font: defaultThemeSettings.buttonFont,
-    fontWeight: "bold",
+  margin: "10px",
+  borderRadius: "10px",
+  color: defaultSettings.buttonTextColor,
+  backgroundColor: defaultSettings.buttonColor,
+  width: "100px",
+  height: "30px",
+  font: defaultSettings.buttonFont,
+  fontWeight: "bold",
 };
+
+/***********************************************************************************
+ *
+ * The default theme settings.
+ *
+ ***********************************************************************************/
+export const defaultThemeSettings = { ...defaultSettings,  buttonStyle: buttonStyle};
 
 
 /*********************************************************************
- * 
+ *
  * 04-23-2020   Initial check-in.
- * 09-16-2020   Added the getNormalBackColor and getErrorColor 
+ * 09-16-2020   Added the getNormalBackColor and getErrorColor
  *              functions.
- * 
+ *
  *********************************************************************/
 
 /********************************************************************************
- * 
+ *
  * This will generate a button and will determine if the button should be
  * disabled or not.
- * 
- * @param {*} style         the style used for the button 
- * @param {*} error         indicates whether an error occurred and if so, disable 
+ *
+ * @param {*} style         the style used for the button
+ * @param {*} error         indicates whether an error occurred and if so, disable
  *                          the button
  * @param {*} disabled      indicates whether the button should be disabled or not
  * @param {*} disableColor  the color for the disabled button (default is gray)
- * 
+ *
  *********************************************************************************/
 export const generateButton = (style, error, disabled = false, disableColor = 'gray') => {
     const genButtonStyle = Object.assign ({}, style); // Copy the button style
@@ -62,7 +72,7 @@ export const generateButton = (style, error, disabled = false, disableColor = 'g
         genButtonStyle.backgroundColor = disableColor;
     } else {    // Do not disable the button
         // Make sure the style has a background color, if not use the theme color
-        genButtonStyle.backgroundColor = (style.hasOwnProperty('backgroundColor') === true) ? 
+        genButtonStyle.backgroundColor = (hasProperty(style, 'backgroundColor') === true) ?
             style.backgroundColor : defaultThemeSettings.backgroundColor;
     }
 
@@ -70,14 +80,14 @@ export const generateButton = (style, error, disabled = false, disableColor = 'g
 }
 
 /********************************************************************************
- * 
+ *
  * This will generate a button and will determine if the button should be
  * disabled or not.
- * 
- * @param {*} error         indicates whether an error occurred and if so, disable 
+ *
+ * @param {*} error         indicates whether an error occurred and if so, disable
  *                          the button
  * @param {*} disabled      indicates whether the button should be disabled or not
- * 
+ *
  *********************************************************************************/
 export const generateDefaultButton = (error, disabled = false) => {
     const genButtonStyle = Object.assign ({}, buttonStyle); // Copy the button style
---
+++ src/gen_imports.js
@@ -0,0 +1,35 @@
+
+const fs = require('fs');
+
+const files = fs.readdirSync('.')
+              .filter(fn => fn.endsWith('.js'))
+              .filter(fn => fn !== 'gen_imports.js')
+              .filter(fn => fn !== 'index.js')
+
+const ans = files.map( fname => {
+  const names = fs.readFileSync(fname,{ encoding : 'utf8' })
+      .split('\n')
+      .filter( line => line.includes('export '))
+      .filter( line => !line.includes('export default'))
+      .map(line => line.split(' ')[2])  // get 3rd word
+
+  return { fname, names:names.sort().join(', ') }
+})
+   .filter(f => f.names.length>0)
+   .map( info => [
+     `import { ${info.names} } from './${info.fname}' `,
+     `export { ${info.names} }`
+   ])
+
+ans.flat().forEach(f => console.log(f))
+
+/*
+import { DatePickerStyle() } from 'DatePickerStyle.js'
+export { DatePickerStyle() }
+import { clearInvalidDual, clearInvalidScreen, clearInvalidScreenOnly, clearInvalidTable, copyStyle, getInvalidMessage, invalidStyling, isConstant, isInvalid, processStyleScreen, processStyleTable, resetDisplayScreen, resetDisplayTable, setInvalidDual, setInvalidScreen, setInvalidTable, validCheckDual, validCheckScreen, validCheckTable, validStyling, wasClickedScreen, wasClickedTable } from 'Invalid.js'
+export { clearInvalidDual, clearInvalidScreen, clearInvalidScreenOnly, clearInvalidTable, copyStyle, getInvalidMessage, invalidStyling, isConstant, isInvalid, processStyleScreen, processStyleTable, resetDisplayScreen, resetDisplayTable, setInvalidDual, setInvalidScreen, setInvalidTable, validCheckDual, validCheckScreen, validCheckTable, validStyling, wasClickedScreen, wasClickedTable }
+import { Choice, List } from 'List.js'
+export { Choice, List }
+import { buttonStyle, defaultThemeSettings, generateButton, generateDefaultButton } from 'Theme.js'
+export { buttonStyle, defaultThemeSettings, generateButton, generateDefaultButton }
+*/
