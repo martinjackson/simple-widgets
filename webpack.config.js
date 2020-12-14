4e24ca3dd9b04f8737e66e8baa1bdea370b6061c
James Abele <James.Abele@fda.hhs.gov>
2020-12-14 15:02:48 -0600

    Matching master with branch.

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,29 @@
 
-const path = require('path');
+const path = require("path");
 
-const pkg = require('./package.json');
-const libraryName= pkg.name;
+const pkg = require("./package.json");
+const libraryName = pkg.name;
 
-module.exports = function(env, argv) {
-
-  return {
-  mode: 'development',
-  devtool: 'source-map',
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
 
   entry: path.join(__dirname, "./src/index.js"),
   target: "web",
   output: {
-    path: path.join(__dirname, 'lib'),
+    publicPath: "/lib/",
+    path: path.join(__dirname, "lib"),
     filename: "index.js",
     library: libraryName,
-    libraryTarget: 'umd',
-    publicPath: '/lib/',
+    libraryTarget: "umd",
     umdNamedDefine: true
   },
   resolve: {
-    modules: [
-      path.join(__dirname, "src"),
-      "node_modules"
-    ]
+    alias: {
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+    },
+    modules: [path.join(__dirname, "src"), "node_modules"],
   },
   stats: "normal",
   module: {
@@ -32,9 +31,9 @@ module.exports = function(env, argv) {
       {
         test: /\.(scss|css)$/,
         use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
+          "style-loader",
+          "css-loader",
+          "sass-loader",
         ],
       },
 
@@ -42,52 +41,47 @@ module.exports = function(env, argv) {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
+          presets: ["@babel/preset-env", "@babel/preset-react"],
           plugins: [
-              '@babel/plugin-proposal-object-rest-spread',
-              [ '@babel/plugin-proposal-class-properties', {"loose": true} ]
-            ]
-          }
-        }
+              "@babel/plugin-proposal-object-rest-spread",
+              "@babel/plugin-proposal-optional-chaining",
+              ["@babel/plugin-proposal-class-properties",
+                  {"loose": true} ],
+            ],
+          },
+        },
       },
 
       // Images
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
 
       // Fonts and SVGs
       {
         test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: 'asset/inline',
+        type: "asset/inline",
       },
+    ],
 
-    ]
   },
-  resolve: {
-    alias: {
-        'react': path.resolve(__dirname, './node_modules/react'),
-        'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
-    }
-},
+
 externals: {
     // Don't bundle react or react-dom
     react: {
         commonjs: "react",
         commonjs2: "react",
         amd: "React",
-        root: "React"
+        root: "React",
     },
     "react-dom": {
         commonjs: "react-dom",
         commonjs2: "react-dom",
         amd: "ReactDOM",
-        root: "ReactDOM"
-    }
-}
-}
-
+        root: "ReactDOM",
+    },
+  },
 }
