232f5714a3e5c793b519399b0cdf9a286cdb5896
James Abele <James.Abele@fda.hhs.gov>
2021-02-05 12:04:22 -0600

    Added error messaging and default props.

--- src/AlertModal.js
+++ src/AlertModal.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Modal from './Modal.js';
-import { buttonStyle } from './Theme.js';
+import { buttonStyle as defButtonStyle} from './Theme.js';
 import './modal.css';
 
-const AlertModal = props => {
+const defProps = { 
+    show: true,
+    closeFunct: () => {},
+    message: 'No Alert message given',
+    buttonStyle: {...defButtonStyle},
+}
+
+const AlertModal = inProps => {
+
+    const props = {...defProps, ...inProps};
+
+    if (('show' in inProps) === false) {
+        console.error ('AlertModal: The show property is not present');
+    }
+
+    if (('closeFunct' in inProps) === false) {
+        console.error ('AlertModal: The closeFunct property is not present');
+    }
+
     return (
         <div>
             {
                 props.show === true ?  (
                     <Modal>
                         <div>
-                            <h1>{props.message}</h1>
-                            <button name="ok" onClick={() => props.closeFunct(false)} style={buttonStyle} >OK</button>
+                            <h1>{(props.message === '') ? defProps.message : props.message}</h1>
+                            <button name="ok" onClick={() => props.closeFunct(false)} style={props.buttonStyle} >OK</button>
                         </div>
                     </Modal>
                 ) : null
