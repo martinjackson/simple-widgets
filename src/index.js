b3acaa82261d4caecd4fcd175ad2fa787cdd1ab3
Martin A. Jackson <martin.a.jackson@gmail.com>
2021-09-02 13:20:40 -0500

    Release 1.12.0

--- src/index.js
+++ src/index.js
@@ -10,20 +10,20 @@ import ConfirmModal from './ConfirmModal.js'
 
 import ContextMenu from './ContextMenu.js'
 
-import  { convertDate, dateTime, addDigit, currentDateTime, currentDate,
+import  { convertDate, dateTime, addDigit, currentDateTime, currentDate, 
 currentDBDateTime, currentDBDate, dbDate, monthName }  from './DateFunct.js'
 
-import DateInput from "./DateInput"
+import DateInput from './DateInput.js'
 
 import DoubleListBox from './DoubleListBox.js'
 
 import ErrorModal from './ErrorModal.js'
 
-import  { generateInvalid, setInvalidScreen, setInvalidTable, setInvalidDual, checkValidityScreen,
-checkValidityTable, resetDisplayScreen, resetDisplayTable, wasClickedScreen, wasClickedTable,
-isConstant, validCheckDual, validCheckScreen, validCheckTable, clearInvalidDual,
-clearInvalidScreen, clearInvalidTable, copyStyle, processInvalidStyleScreen, processStyleScreen,
-clearInvalidScreenOnly, processInvalidStyleTable, processStyleTable, validStyling, invalidStyling,
+import  { generateInvalid, setInvalidScreen, setInvalidTable, setInvalidDual, checkValidityScreen, 
+checkValidityTable, resetDisplayScreen, resetDisplayTable, wasClickedScreen, wasClickedTable, 
+isConstant, validCheckDual, validCheckScreen, validCheckTable, clearInvalidDual, 
+clearInvalidScreen, clearInvalidTable, copyStyle, processInvalidStyleScreen, processStyleScreen, 
+clearInvalidScreenOnly, processInvalidStyleTable, processStyleTable, validStyling, invalidStyling, 
 isInvalid, getInvalidMessage }  from './Invalid.js'
 
 import  { Choice, List }  from './List.js'
@@ -38,21 +38,27 @@ import SearchSortTable from './SearchSortTable.js'
 
 import  { buttonStyle, defaultThemeSettings, generateButton, generateDefaultButton }  from './Theme.js'
 
+import  { isOpera, isFirefox, isSafari, isIE, isEdge, 
+isChrome, isEdgeChromium, isBlink }  from './browserDetect.js'
+
+import  { date2str }  from './date2str.js'
+
+import  { localStrToDate }  from './localStrToDate.js'
 
 import makeChangeHandler from './makeChangeHandler.js'
 
-
-export { AlertModal, CheckBox, ChoiceText, sanitize, formatMoney,
-ConfirmModal, ContextMenu, convertDate, dateTime, addDigit,
-currentDateTime, currentDate, currentDBDateTime, currentDBDate, dbDate, monthName,
-DateInput,
-DoubleListBox, ErrorModal,
-generateInvalid, setInvalidScreen, setInvalidTable, setInvalidDual, checkValidityScreen,
-checkValidityTable, resetDisplayScreen, resetDisplayTable, wasClickedScreen, wasClickedTable,
-isConstant, validCheckDual, validCheckScreen, validCheckTable, clearInvalidDual,
-clearInvalidScreen, clearInvalidTable, copyStyle, processInvalidStyleScreen, processStyleScreen,
-clearInvalidScreenOnly, processInvalidStyleTable, processStyleTable, validStyling, invalidStyling,
-isInvalid, getInvalidMessage, Choice, List, Modal,
-Radio, search, binSearch, SearchSortTable, buttonStyle,
-defaultThemeSettings, generateButton, generateDefaultButton, makeChangeHandler }
+export { AlertModal, CheckBox, ChoiceText, sanitize, formatMoney, 
+ConfirmModal, ContextMenu, convertDate, dateTime, addDigit, 
+currentDateTime, currentDate, currentDBDateTime, currentDBDate, dbDate, 
+monthName, DateInput, DoubleListBox, ErrorModal, generateInvalid, 
+setInvalidScreen, setInvalidTable, setInvalidDual, checkValidityScreen, checkValidityTable, 
+resetDisplayScreen, resetDisplayTable, wasClickedScreen, wasClickedTable, isConstant, 
+validCheckDual, validCheckScreen, validCheckTable, clearInvalidDual, clearInvalidScreen, 
+clearInvalidTable, copyStyle, processInvalidStyleScreen, processStyleScreen, clearInvalidScreenOnly, 
+processInvalidStyleTable, processStyleTable, validStyling, invalidStyling, isInvalid, 
+getInvalidMessage, Choice, List, Modal, Radio, 
+search, binSearch, SearchSortTable, buttonStyle, defaultThemeSettings, 
+generateButton, generateDefaultButton, isOpera, isFirefox, isSafari, 
+isIE, isEdge, isChrome, isEdgeChromium, isBlink, 
+date2str, localStrToDate, makeChangeHandler }
 
