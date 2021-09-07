85774fa772ea91c2648be69398f3d9f84b68b267
Martin A. Jackson <martin.a.jackson@gmail.com>
2021-09-07 13:27:13 -0500

    Release 1.12.1

--- webpack.config.js
+++ webpack.config.js
@@ -45,8 +45,9 @@ module.exports = {
           plugins: [
               "@babel/plugin-proposal-object-rest-spread",
               "@babel/plugin-proposal-optional-chaining",
-              ["@babel/plugin-proposal-class-properties",
-                  {"loose": true} ],
+              ["@babel/plugin-proposal-class-properties", {"loose": true} ],
+              ["@babel/plugin-proposal-private-property-in-object", { "loose": true }],
+              ["@babel/plugin-proposal-private-methods", { "loose": true }]
             ],
           },
         },
