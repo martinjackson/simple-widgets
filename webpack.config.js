9b278361259b77004fc1a25d5e59e6d83d8dc5e9
Martin A. Jackson <martin.a.jackson@gmail.com>
2021-10-05 12:12:10 -0500

    removed css processing from webpack -- causing some CSS rules to get mangled

--- webpack.config.js
+++ webpack.config.js
@@ -31,16 +31,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [ "style-loader", "css-loader" ],
-      },
-
-      {
-        test: /\.scss$/,
-        use: [ "style-loader", "css-loader", "sass-loader", ],
-      },
-
-      {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
