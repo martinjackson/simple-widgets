4965a50ae8b8d1aedba0f4b58ffe50169a0e50bb
Martin A. Jackson <martin.a.jackson@gmail.com>
2021-10-05 12:17:27 -0500

    Release 1.15.0

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: [ "style-loader", "css-loader" ],
+      },
+
+      {
+        test: /\.scss$/,
+        use: [ "style-loader", "css-loader", "sass-loader", ],
+      },
+
+      {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
