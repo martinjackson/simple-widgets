0bd869fcda41b61b22e8c72c72c2a1e9c14e1823
Martin A. Jackson <martin.a.jackson@gmail.com>
2021-10-06 08:37:03 -0500

    Release 1.17.1

--- src/ContextMenu.js
+++ src/ContextMenu.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import { defaultThemeSettings } from './Theme';
-// import './contextMenuHover.css';
+
 
 const ContextMenu = (propsIn) => {
     console.log ('props', propsIn);
