5ad9ae66da7febd8cea88d04a737b74a63c1c09f
jimabele <james.abele@fda.hhs.gov>
2021-11-04 07:53:51 -0500

    Added the slider.

---
+++ src/Slider.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react'
+
+const Slider = (props) => {
+    const min = (props.hasOwnProperty('start')) ? props.start : 0;
+    const max = (props.hasOwnProperty('end')) ? props.end : 100;
+    const startValue = (props.hasOwnProperty('startValue')) ? props.startValue : 0;
+    const displayValue = (props.hasOwnProperty('noshow')) ? false : true;
+
+    const [rangeValue, setRangeValue] = useState(startValue);
+
+    return (
+        <div className="rangeslider">
+            <input type="range" className="range" name="slider" min={min} defaultValue={startValue} max={max} onChange={(event) => setRangeValue(event.target.value)} onMouseMove={(event) => setRangeValue(event.target.value)} />
+            { (displayValue === true) ? 
+                <span id="id_rangeValue" name="rangeValue" value={rangeValue} className="rangeValue">
+                    {rangeValue}
+                </span> : null
+            }
+        </div>
+    )
+}
+
+export default Slider;
