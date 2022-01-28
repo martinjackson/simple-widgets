390d6ac6894cea939e4cfb974e82b6b0b8e5ad52
Martin A. Jackson <martin.a.jackson>
2022-01-27 20:53:09 -0600

    after correcting more low level issues reported by DeepScan

--- src/InputFile.js
+++ src/InputFile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 const hasProperty = (obj, propName) => { return !!Object.getOwnPropertyDescriptor(obj, propName);}
 
