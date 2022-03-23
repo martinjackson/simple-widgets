6c5298b94eddba9da6fcfcc654a94ae048703713
Martin A. Jackson <Martin.Jackson@fda.hhs.gov>
2022-03-23 13:46:40 -0500

    testing <forms/Choice sorting opt[]s keys by value

--- src/forms/List.js
+++ src/forms/List.js
@@ -1,5 +1,41 @@
 import React, { Fragment } from 'react';
 
+/*
+
+  TODO:  Handle values other than Strings or something that converts to strings
+
+  currently supports:
+
+  const userList = {0: 'Vacancy, null', 2: 'Hernandez, April', 128: 'Lemon, Brian', 901: 'NCTR, Demo', 1500: 'Rice, Antonie', 1501: 'Woody, Jameca' …}
+
+     <Choice name="NCTR_ID" choices={userList} value=2/>
+
+  OR:
+
+  const userList = ['Vacancy, null','Hernandez, April','Lemon, Brian','NCTR, Demo','Rice, Antonie','Woody, Jameca','Smith, Sherry','Brooks, Roben','Monk, Mary', ... ]
+
+     <Choice name="NCTR_ID" choices={userList} value="Hernandez, April"/>
+*/
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------
+const compareString = (a,b) => {
+   if (a < b)  return -1
+   if (a > b)  return 1
+
+   return 0
+}
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------
+const sortKeysByValue = (obj) => {
+  const keys = Object.keys(obj)
+
+  const list = keys.map(k => { return {id: k, value: obj[k]} })
+                   .sort( (a,b) => compareString(a.value, b.value))
+
+  return list.map(item => item.id)
+}
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------
 export const Choice = (propsIn) => {
 
     const {list, choices, size, ...props} = propsIn
@@ -7,12 +43,12 @@ export const Choice = (propsIn) => {
     const opt = list || choices || []
 
     const isKeyed = ( !Array.isArray(opt) )
-    const keys = (isKeyed) ? Object.keys(opt) : opt
+    const keys = (isKeyed) ? sortKeysByValue(opt) : opt     // origianlly       const keys = (isKeyed) ? Object.keys(opt) : opt
 
     const genOption = (el, k) => {
       if (isKeyed) {
-        return (opt[el] == props.value) ?        
-          <option key={k} value={el} selected={true}>{opt[el]}</option> :
+        return (opt[el] == props.value) ?
+          <option key={k} value={el} selected={true}>{opt[el]}</option> :                 // <option key="1" value="2" selected={true}>'Hernandez, April'</option>
           <option key={k} value={el}>{opt[el]}</option>
       } else {
         return (el == props.value) ?
