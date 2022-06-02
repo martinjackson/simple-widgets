cb1aaa77661bdf04593745a26b0c045c7eb5a564
Martin A. Jackson <martin.a.jackson@gmail.com>
2022-06-02 14:28:26 -0500

    fixed getList() used by <Header when there is no .env list

--- src/encrypt.js
+++ src/encrypt.js
@@ -1,14 +1,26 @@
 import CryptoJS from 'crypto-js'
 
-export const getList = () => {    
+export const getList = () => {
+    if (!window.env.list) {
+       return []
+    }
+       
     return decrypt(window.env.list).split(',')
 }
 
 export const decrypt = (msg) => {
+    if (!window.env.ekey) {
+       return msg
+    }
+
     return CryptoJS.AES.decrypt(msg, window.env.ekey).toString(CryptoJS.enc.Utf8)
 }
 
 export const encrypt = (msg) => {
-    return CryptoJS.AES.encrypt(msg, window.env.ekey).toString();
+    if (!window.env.ekey) {
+        return msg
+     }
+     
+     return CryptoJS.AES.encrypt(msg, window.env.ekey).toString();
 }
 
