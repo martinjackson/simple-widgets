23e31bdb08d66d4b57da5998ce9340d06aae3017
Martin A. Jackson <martin.a.jackson@gmail.com>
2023-01-01 11:58:32 -0600

    Update stage.sh

--- misc/stage.sh
+++ misc/stage.sh
@@ -2,6 +2,7 @@
 
 if [ -z "$(git status --untracked-files=no --porcelain)" ]; then
   # Working directory clean excluding untracked files
+  :
 else
   # Uncommitted changes in tracked files
   git commit --all  --allow-empty -q -m staged-for-production
