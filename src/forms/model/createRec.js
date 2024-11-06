33e6f0aaef65715fc9ea13ea2087ddd6a83b70b2
Martin A. Jackson <Martin.Jackson@fda.hhs.gov>
2024-11-06 00:04:02 -0600

    a work in program, needs much testing.

--- src/forms/model/createRec.js
+++ src/forms/model/createRec.js
@@ -14,19 +14,20 @@ const INSERT_RECORD = gql`mutation(
 `
 
 // ------------------------------------------------------------------------
-export const createRec = (gqlTable, input, where, username) => {
+export const createRec = (gqlTable, input) => {
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [createRecord] = useMutation(INSERT_RECORD, {client, fetchPolicy: 'network-only'});
 
-    createRecord({ variables: { gqlTable: gqlTable, input: input, where: where, who: username } })
+    // TODO: this function should return the promise and make the calling code cleaner
+    createRecord({ variables: { gqlTable: gqlTable, input: input} })
         .then(rec => {
           const status = rec.data.createRecord;
           if (status) {
-              console.log('record inserted:', where);
+              console.log('record inserted:', input);
           }
         })
         .catch(err => {
-          throw new Error('Unable to insert ' + where + ' -- ' + err);
-        });
+          throw new Error('Unable to insert ' + input + ' -- ' + err);
+        })
 }
