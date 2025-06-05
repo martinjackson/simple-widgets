3e69e146a298220efeacd1db6a4ce014efd0fd8f
Martin A. Jackson <Martin.Jackson@fda.hhs.gov>
2025-06-05 08:13:59 -0500

    renamed all .jsx to js, prep for branch compare

---
+++ src/rename.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Find all .jsx files and rename them to .js
+
+
+# for file in $(git ls-files '*.jsx'); do
+
+cd ~/projects/simple-widgets/src/
+
+for file in $(ls *.jsx); do
+  # Get the base name without the extension
+  base_name=$(basename "$file" .jsx)
+
+  # Get the directory name
+  dir_name=$(dirname "$file")
+
+  # Construct the new file name with .js extension
+  new_file="$dir_name/$base_name.js"
+
+  # Use git mv to rename the file
+  git mv "$file" "$new_file"
+done
+
+# Commit the changes
+echo git commit -m "Rename all .jsx files to .js"
+
