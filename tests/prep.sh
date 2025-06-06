6f50be851550cf777e24a13525775c3a617d961f
Martin A. Jackson <Martin.Jackson@fda.hhs.gov>
2025-06-06 11:07:50 -0500

    mid-generation of unit tests

--- tests/prep.sh
+++ tests/prep.sh
@@ -1,4 +1,5 @@
 #!/bin/dash
 
 npm install --save-dev jest @testing-library/react @testing-library/jest-dom
+npm install --save-dev jest @babel/core @babel/preset-env @babel/preset-react babel-jest
 
