9fe3398aab99c121f6ea8637034ce78c473e8848
Martin A. Jackson <Martin.a.Jackson@gmail.com>
2025-12-16 09:09:44 -0600

    updated pub.sh to match what I just did manually.

--- pub.sh
+++ pub.sh
@@ -3,4 +3,10 @@
 # git add lib/*.css
 # git commit -am 'added lib/*.css'
 
-npm run pub
+# npm run pub
+
+npm run build
+
+echo "have your browser and phone ready..."
+npm login
+npx release-it --no-git.requireCleanWorkingDir --npm.skipChecks
